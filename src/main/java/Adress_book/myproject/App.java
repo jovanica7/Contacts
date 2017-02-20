b940287259d66b1abc41b316ed6db6f6d12e334b
Jovana Vajagic <Jovana Vajagic>
2017-02-21 00:06:59 +0100

    added my project

---
+++ src/main/java/Adress_book/myproject/App.java
@@ -0,0 +1,13 @@
+package Adress_book.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
